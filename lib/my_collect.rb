655e602bdca54dac632f7e4f41d841b6514280a2
Jordan Eckowitz <jordan.eckowitz@gmail.com>
2018-05-04 20:46:19 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+    i = 0
+    item = []
+    while i < array.length
+      if array[i].include? " "
+        item[i] = array[i].split(" ").first
+      else 
+        item[i] = array[i].upcase
+      end
+      yield(item[i])
+      i = i + 1
+    end
+    item
+end
